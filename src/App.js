5358f59ae13fcf8f89e03ca23135bca5d1cb88d4
MasonBoom <masonboom@gmail.com>
2023-05-14 09:30:20 -0400

    added header and icon img

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
-      <h1>React App</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <h1>Image Identification</h1>
+    </>
+  );
+}
